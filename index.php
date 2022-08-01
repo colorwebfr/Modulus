35849ff336408098c5b9fbb6f033d1e2006df16d
Kevin <capoccettiwebdev@gmail.com>
2022-08-01 17:58:41 +0100

    Last Version de modulus pour 01-08-2022

--- index.php
+++ index.php
@@ -6,35 +6,105 @@
 
 <?php include $_SERVER['DOCUMENT_ROOT']."/modulus/incl/header.php"; ?>
 
+    <div class="main_home">
 
-    <div class="page-section">
-		<div class="container">
-			<div class="row">		
-				<div class="col-md-6 left-50">
-					<div class="fes2-main-text-cont">
-						<div class="title-fs-45">
-								OPTIMIZED FOR<br>
-							<span class="bold">MOBILE</span>
+        <!-- Section Better Play -->
+        <div class="page-section">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 wow fadeInLeft equal-height" style="visibility: visible; animation-name: fadeInLeft;">
+						<div class="fes2-main-text-cont">
+							<div class="title-fs-45">
+								MORE THAN<br>
+								<span class="bold">RESPONSIVE</span>
+							</div>
+							<div class="line-3-70"></div>
+							<div class="fes2-text-cont">Sed ut perspiciatis unde omnis iste nat eror acus  antium que. Asperiores, ea velit enim labore doloribus.
+                            </div>
 						</div>
-						<div class="line-3-100"></div>
-						<div class="fes2-text-cont">Sed ut perspiciatis unde omnis iste nat eror acus  antium que. Asperiores, ea velit enim labore doloribus.
-                        </div>
-                        <div class="mt-30">
-                            <a class="button medium thin hover-dark" href="https://1.envato.market/GVZ26">BUY NOW</a>
-                        </div>
+					</div>
+					<div class="col-md-6">
+						<div class="row">
+							<div class="fes2-img equal-height"></div>
+						</div>
 					</div>
 				</div>
-              
-                <div class="col-md-6 right-50 wow fadeInLeft" style="visibility: visible; animation-name: fadeInLeft;">	
-					<div class="ads-img-cont">
-                        <img src="images/ads-phone.jpg" alt="img">
-                    </div>
-			    </div>
-              
 			</div>
 		</div>
-	</div>
+        <!-- END Section Better Play -->
+        <!-- Section Grow Revenues -->
+        <div class="page-section">
+		    <div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 left-50 wow fadeInRight equal-height" style="visibility: visible; animation-name: fadeInRight;">
+						<div class="fes2-main-text-cont">
+							<div class="title-fs-45">
+								POWERFUL<br>
+								<span class="bold">PERFORMANCE</span>
+							</div>
+							<div class="line-3-100"></div>
+							<div class="fes2-text-cont">Sed ut perspiciatis unde omnis iste nat eror acus  antium que. Asperiores, ea velit enim labore doloribus.
+                            </div>
+						</div>
+					</div>
+                    <div class="col-md-6 right-50">
+						<div class="row">
+							<div class="fes3-img equal-height"></div>
+						</div>
+					</div>
+                </div>
+			</div>
+		</div>
+        <!-- END Section Grow Revenues -->
+                <!-- Section More Value -->
+                <div class="page-section">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 wow fadeInLeft equal-height" style="visibility: visible; animation-name: fadeInLeft;">
+						<div class="fes2-main-text-cont">
+							<div class="title-fs-45">
+								MORE THAN<br>
+								<span class="bold">RESPONSIVE</span>
+							</div>
+							<div class="line-3-70"></div>
+							<div class="fes2-text-cont">Sed ut perspiciatis unde omnis iste nat eror acus  antium que. Asperiores, ea velit enim labore doloribus.
+                            </div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="row">
+							<div class="fes2-img equal-height"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+        <!-- END Section More Value -->
+        <!-- Section Customer -->
+        <div class="page-section">
+		    <div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 left-50 wow fadeInRight equal-height" style="visibility: visible; animation-name: fadeInRight;">
+						<div class="fes2-main-text-cont">
+							<div class="title-fs-45">
+								POWERFUL<br>
+								<span class="bold">PERFORMANCE</span>
+							</div>
+							<div class="line-3-100"></div>
+							<div class="fes2-text-cont">Sed ut perspiciatis unde omnis iste nat eror acus  antium que. Asperiores, ea velit enim labore doloribus.
+                            </div>
+						</div>
+					</div>
+                    <div class="col-md-6 right-50">
+						<div class="row">
+							<div class="fes3-img equal-height"></div>
+						</div>
+					</div>
+                </div>
+			</div>
+		</div>
+        <!-- END Section Customer -->
 
-    
+    </div>
 
 <?php include $_SERVER['DOCUMENT_ROOT']."/modulus/incl/footer.php"; ?>
